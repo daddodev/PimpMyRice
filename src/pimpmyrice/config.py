cf5720740c088734cff48ca5b5010a2b1a79fed8
daddodev <daddodev@gmail.com>
2024-10-17 00:36:05 +0200

    change config dir

--- src/pimpmyrice/config.py
+++ src/pimpmyrice/config.py
@@ -23,7 +23,7 @@ match sys.platform:
         CLIENT_OS = Os.MAC
         CONFIG_DIR = HOME_DIR / "Library/Application Support"
 
-PIMP_CONFIG_DIR = CONFIG_DIR / "pimpmyrice"
+PIMP_CONFIG_DIR = HOME_DIR / "pimpmyrice"
 PIMP_DIR = Path(os.path.abspath(os.path.join(__file__, "../../")))
 VENV_DIR = PIMP_CONFIG_DIR / "venv"
 
@@ -39,11 +39,11 @@ THEMES_DIR = PIMP_CONFIG_DIR / "themes"
 STYLES_DIR = PIMP_CONFIG_DIR / "styles"
 PALETTES_DIR = PIMP_CONFIG_DIR / "palettes"
 MODULES_DIR = PIMP_CONFIG_DIR / "modules"
-LOCK_FILE = PIMP_CONFIG_DIR / "pimpmyrice.lock"
-CORE_PID_FILE = PIMP_CONFIG_DIR / "core.pid"
-SERVER_PID_FILE = PIMP_CONFIG_DIR / "server.pid"
-LOG_FILE = PIMP_CONFIG_DIR / "pimpmyrice.log"
 TEMP_DIR = PIMP_CONFIG_DIR / ".tmp/"
+LOCK_FILE = TEMP_DIR / "pimpmyrice.lock"
+CORE_PID_FILE = TEMP_DIR / "core.pid"
+SERVER_PID_FILE = TEMP_DIR / "server.pid"
+LOG_FILE = PIMP_CONFIG_DIR / "pimpmyrice.log"
 REPOS_LIST = PIMP_CONFIG_DIR / "remote_repos" / "list.txt"
 
 REPOS_BASE_ADDR = "https://github.com/pimpmyrice-modules"
