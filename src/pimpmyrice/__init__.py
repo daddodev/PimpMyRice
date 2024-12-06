19c3ccc184e6950bc3da49441e31afaa9d2ba27c
daddodev <daddodev@gmail.com>
2024-12-06 08:53:11 +0100

    add __version__

--- src/pimpmyrice/__init__.py
+++ src/pimpmyrice/__init__.py
@@ -1,5 +1,6 @@
 import os
 import pkgutil
+from importlib.metadata import version
 
 from pimpmyrice.files import check_config_dirs
 from pimpmyrice.logger import set_up_logging
@@ -8,5 +9,6 @@ __all__ = [name for _, name, _ in pkgutil.iter_modules([os.path.dirname(__file__
 for module in __all__:
     __import__(f"{__name__}.{module}")
 
+__version__ = version("pimpmyrice")
 
 set_up_logging()
